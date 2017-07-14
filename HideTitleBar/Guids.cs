b659d90ce67840dd30cc52d1fc5aaa458d5a4a17
db <rocky_ting@qq.com>
2017-07-14 17:46:21 +0800

    Version 1.2.2 Src

---
+++ HideTitleBar/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.HideTitleBar
+{
+    static class GuidList
+    {
+        public const string guidHideTitleBarPkgString = "5fa47fb4-6814-4058-a2ac-ca3157d9b654";
+        public const string guidHideTitleBarCmdSetString = "0f74c51d-0aae-43c2-9730-0ff6bd141541";
+
+        public static readonly Guid guidHideTitleBarCmdSet = new Guid(guidHideTitleBarCmdSetString);
+    };
+}
---
+++ HideTitleBar/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Company.HideTitleBar
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidHideTitleBar =        0x100;
+
+
+    };
+}
